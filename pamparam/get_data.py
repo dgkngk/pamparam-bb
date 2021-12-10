0ace49566a11d19ce052353b093a21e9c304de2c
dgkngk <dogukan_gok@outlook.com>
2021-12-10 23:06:56 +0300

    modified:get_data.py for some pep corrections

--- pamparam/get_data.py
+++ pamparam/get_data.py
@@ -1,52 +1,60 @@
-import config, threading,time
+import config
+import threading
+import time
 from binance.client import Client
 from tafunc import ta_analyze
 from collections import OrderedDict
-#@todo: add overload for user selection of data intervals
-#this function gets the kline data provided in the binance api
+# @todo: add overload for user selection of data intervals
+# this function gets the kline data provided in the binance api
+
+
 def get_klines(selected_interval):
-    client = Client(config.API_KEY,config.API_SECRET)
-    
-    #get the possible exchanges for the quoting asset, that are spot tradeable
+    client = Client(config.API_KEY, config.API_SECRET)
+
+    # get the possible exchanges for the quoting asset, that are spot tradeable
     exinfo = client.get_exchange_info()
     exlist = []
-    
+
     for symbol in exinfo["symbols"]:
-        if((symbol["quoteAsset"] == "USDT") & (symbol["isSpotTradingAllowed"] == True) & (symbol["isMarginTradingAllowed"])==True):
+        if((symbol["quoteAsset"] == "USDT")
+            & (symbol["isSpotTradingAllowed"] is True)
+                & (symbol["isMarginTradingAllowed"]) is True):
             exlist.append(symbol["symbol"])
 
-    #getting the kline data to analyse for the exchangeable assets
+    # getting the kline data to analyse for the exchangeable assets
     klines = {}
     unsorted = {}
     if selected_interval == "1HOUR":
         s_interval = Client.KLINE_INTERVAL_1HOUR
-    elif selected_interval =="15MIN":
+    elif selected_interval == "15MIN":
         s_interval = Client.KLINE_INTERVAL_15MINUTE
-    elif selected_interval =="4HOUR":
+    elif selected_interval == "4HOUR":
         s_interval = Client.KLINE_INTERVAL_4HOUR
-    elif selected_interval =="1DAY":
+    elif selected_interval == "1DAY":
         s_interval = Client.KLINE_INTERVAL_1DAY
-    elif selected_interval =="1MIN":
+    elif selected_interval == "1MIN":
         s_interval = Client.KLINE_INTERVAL_1MINUTE
     else:
         s_interval = Client.KLINE_INTERVAL_4HOUR
-        
-     #@todo:multithreading here, done
+
+    # @todo:multithreading here, done
 
     threadLock = threading.Lock()
     threads = []
     i = 0
     for exchange in exlist:
         i = i + 1
-        # candledata = []#one of my ultimate bruh moments, it shall remain here as a memento mori
+        # candledata = []#one of my ultimate bruh moments,
+        # it shall remain here as a memento mori
         try:
             print(exchange)
-            new_thread = threading.Thread(target=apicall_thread, args=(client, exchange, s_interval, unsorted, klines, threadLock)) 
+            new_thread = threading.Thread(target=apicall_thread, args=(
+                client, exchange, s_interval, unsorted, klines, threadLock))
             new_thread.start()
             threads.append(new_thread)
 
-            #not bruh moments
-            # candledata = (client.get_klines(symbol = exchange, interval = s_interval, limit = 100))
+            # not bruh moments
+            # candledata = (client.get_klines(*yadayada*))
             # klines[exchange] = candledata
             # unsorted[exchange] = ta_analyze(candledata)
         except Exception as e:
@@ -55,31 +63,37 @@ def get_klines(selected_interval):
 
     for t in threads:
         t.join()
-            
-    #sorting with lambda black magic fuckery
-    sortedsigs = dict(OrderedDict(sorted(unsorted.items(), key= lambda t:t[1]['signal'])))
-    print (str(i) + " coins analysed and sorted")
-    
+
+    # sorting with lambda black magic fuckery
+    sortedsigs = dict(OrderedDict(
+        sorted(unsorted.items(), key=lambda t: t[1]['signal'])))
+    print(str(i) + " coins analysed and sorted")
+
     return (sortedsigs)
 
-def get_one_kline():#trial method for getting kline data
-    client = Client(config.API_KEY,config.API_SECRET)
-    
+
+def get_one_kline():  # trial method for getting kline data
+    client = Client(config.API_KEY, config.API_SECRET)
+
     try:
-        a = (client.get_klines(symbol = "DOGEUSDT", interval = Client.KLINE_INTERVAL_15MINUTE, limit = 1))
-        
-        dct = dict(zip(range(12),a))
+        a = (client.get_klines(symbol="DOGEUSDT",
+                               interval=Client.KLINE_INTERVAL_15MINUTE,
+                               limit=1))
+
+        dct = dict(zip(range(12), a))
         return dct
-    
+
     except Exception as e:
         print(e)
         return("nah")
 
+
 def apicall_thread(client, exchange, s_interval, unsorted, klines, lock):
-    #parellelized part
-    candledata = (client.get_klines(symbol = exchange, interval = s_interval, limit = 100))
+    # parellelized part
+    candledata = (client.get_klines(
+        symbol=exchange, interval=s_interval, limit=100))
     ta_result = ta_analyze(candledata)
-    #mutex
+    # mutex
     lock.acquire()
     klines[exchange] = candledata
     unsorted[exchange] = ta_result
